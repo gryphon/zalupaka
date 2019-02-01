94f03a592ee721f1e284dcb9f771caa0a8a92dbc
gryphon <mail@grigor.io>
2019-02-01 15:46:25 +0300

    First release

--- bin/deploy_production.sh
+++ bin/deploy_production.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-scp docker-compose.prod.yml zalupaka:/home/gryphon/docker-compose.prod.yml
-scp config/traefik/traefik.toml zalupaka:/home/gryphon/traefik.toml
-# scp -r config/ssl zalupaka:/home/gryphon/ssl
-
-ssh zalupaka "sudo docker stack deploy -c /home/gryphon/docker-compose.prod.yml zalupaka --with-registry-auth"
+ansible-playbook config/playbook/production.yml -i config/playbook/hosts --tags=deploy
