5d8babf6f5d84b0fbbf818ff6678c0505bef2228
gryphon <mail@grigor.io>
2019-02-03 21:27:26 +0300

    Fix opus encoder paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-#Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.wasm] 
